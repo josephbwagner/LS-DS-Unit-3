49626f99ca14386e73d5c2400f050d878368d3e8
Joseph Wagner <j.wagner1024@gmail.com>
2019-05-27 19:37:09 -0400

    Initialize directory with cookiecutter

--- lambdata_beverast/__init__.py
+++ lambdata_beverast/__init__.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 lambdata - a collection of Data Science helper functions
 """
--- lambdata_beverast/example_module.py
+++ lambdata_beverast/example_module.py
@@ -1,4 +1,11 @@
+from subprocess import run
+
+
 x = 2
 
 def increment(number):
   return number + 1
+
+def initialize_directory():
+  run('cookiecutter https://github.com/drivendata/cookiecutter-data-science')
+
