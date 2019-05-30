26f289934c64181b8993379a391acf8ce6146af3
Joseph <j.wagner1024@gmail.com>
2019-05-30 18:04:18 -0400

    Add pytest test for Directory class

---
+++ lambdata_beverast/test_structurizer.py
@@ -0,0 +1,8 @@
+from . import structurizer
+import os
+
+def test_apply_cookiecutter(tmpdir):
+  dir = structurizer.Directory(tmpdir)
+  dir.apply_cookiecutter()
+  print(os.listdir(tmpdir))
+  assert 'Makefile' in os.listdir(tmpdir)
