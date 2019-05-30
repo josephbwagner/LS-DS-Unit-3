4c3d244dd5fc50c6a69c5a81a89a5ab6acd93375
Joseph <j.wagner1024@gmail.com>
2019-05-30 18:10:38 -0400

    Format repo using Black

--- lambdata_beverast/test_structurizer.py
+++ lambdata_beverast/test_structurizer.py
@@ -1,8 +1,9 @@
 from . import structurizer
 import os
 
+
 def test_apply_cookiecutter(tmpdir):
-  dir = structurizer.Directory(tmpdir)
-  dir.apply_cookiecutter()
-  print(os.listdir(tmpdir))
-  assert 'Makefile' in os.listdir(tmpdir)
+    dir = structurizer.Directory(tmpdir)
+    dir.apply_cookiecutter()
+    print(os.listdir(tmpdir))
+    assert "Makefile" in os.listdir(tmpdir)
