0f6b781c2fd72f9f33271e04a9c006d21d312bfa
Joseph <j.wagner1024@gmail.com>
2019-06-18 20:49:01 -0400

    Add lecture and assignment tasks

--- twittercomparator/app.py
+++ twittercomparator/app.py
@@ -1,19 +1,28 @@
 """
 Main application and routing logic for Twitter-Comparator
 """
+from decouple import config
 from flask import Flask
 from .models import DB
-from decouple import config
+from .twitter import add_or_update_user
+
 
 def create_app():
     """Create and configure an instance of the Flask application"""
     app = Flask(__name__)
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["SQLALCHEMY_DATABASE_URI"] = config("DATABASE_URL")
     DB.init_app(app)
 
     @app.route("/")
     def root():
-        return "Thank you and welcome to the Twitter Comparator."
+        return "Welcome to the Twitter Comparator."
+
+    @app.route("/reset")
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return "DB reset!"
+
 
     return app
--- twittercomparator/twitter.py
+++ twittercomparator/twitter.py
@@ -2,20 +2,46 @@
 Retrieve tweets, embeddings, and persist in the database.
 """
 import basilica
+from decouple import config
 import tweepy
 from .models import DB, Tweet, User
 
 
-TWITTER_AUTH = tweepy.OAuthHandler(getenv('TWITTER_CONSUMER_KEY'),
-                                  getenv('TWITTER_CONSUMER_SECRET'))
-TWITTER_AUTH.set_access_token(getenv('TWITTER_ACCESS_TOKEN'),
-                             getenv('TWITTER_ACCESS_TOKEN_SECRET'))
+TWITTER_AUTH = tweepy.OAuthHandler(config('TWITTER_CONSUMER_KEY'),
+                                  config('TWITTER_CONSUMER_SECRET'))
+TWITTER_AUTH.set_access_token(config('TWITTER_ACCESS_TOKEN'),
+                             config('TWITTER_ACCESS_TOKEN_SECRET'))
 TWITTER = tweepy.API(TWITTER_AUTH)
-BASILICA = basilica.Connection(getenv('BASILICA_KEY'))
+BASILICA = basilica.Connection(config('BASILICA_KEY'))
+
 
 def add_or_update_user(username):
     """
     Add or update a user and their tweets. 
     Return an error if not a Twitter user.
+    
+    Parameters
+    ---------------
+    username (User.screen_name): Twitter identifier
+
+    Returns
+    ---------------
+    None - writes to DB
     """
+    try:
+        twitter_user = TWITTER.get_user(username)
+        tweets = twitter_user.timeline(count=200, exclude_replies=True, include_rts=False, tweet_mode='extended')
+        db_user = User(id=twitter_user.id, name=twitter_user.screen_name, newest_tweet_id=tweets[0].id)
+
+        for tweet in tweets:
+            embedding = BASILICA.embed_sentence(tweet.full_text, model='twitter')
+            db_tweet = Tweet(id=tweet.id, text=tweet.full_text[:500], embedding=embedding)
+            DB.session.add(db_tweet)
+            db_user.tweets.append(db_tweet)
+
+        DB.session.add(db_user)
+        DB.session.commit()
+
+    except Exception as e:
+        print('The following exception occured: ', e)
     
