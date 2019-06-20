941f715a840606418b972d38548737d3d7cd1727
Joseph <j.wagner1024@gmail.com>
2019-06-20 12:01:02 -0400

    Add yesterday's lecture and assignment work

--- twittercomparator/app.py
+++ twittercomparator/app.py
@@ -2,27 +2,52 @@
 Main application and routing logic for Twitter-Comparator
 """
 from decouple import config
-from flask import Flask
-from .models import DB
+from flask import Flask, render_template, request
+from .models import DB, User
+from .predict import predict_user
 from .twitter import add_or_update_user
 
 
 def create_app():
     """Create and configure an instance of the Flask application"""
     app = Flask(__name__)
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["SQLALCHEMY_DATABASE_URI"] = config("DATABASE_URL")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    
     DB.init_app(app)
+
 
     @app.route("/")
     def root():
-        return "Welcome to the Twitter Comparator."
-
-    @app.route("/reset")
-    def reset():
-        DB.drop_all()
-        DB.create_all()
-        return "DB reset!"
+        users = User.query.all()
+        return render_template('base.html',title='Home',users=users)
 
 
+    @app.route("/user", methods=["POST"])
+    @app.route("/user/<name>", methods=["GET"])
+    def user(name=None):
+        name = name or request.values["user_name"]
+        try:
+            if request.method == "POST":
+                add_or_update_user(name)
+                message = "User {} successfully added!".format(name)
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = "Error adding {}: {}".format(name, e)
+            tweets = []
+        return render_template("user.html", title=name, tweets=tweets, message=message)
+
+
+    @app.route("/compare", methods=["POST"])
+    def compare():
+        # Consider making a one-liner with list-unpacking
+        user1 = request.values['user1']
+        user2 = request.values['user2']
+        
+        if user1 == user2:
+            return 'Cannot compare same account!'
+        else:
+            prediction = predict_user(user1, user2, request.values['tweet_text'])
+            return user1 if prediction else user2
+    
     return app
--- twittercomparator/twitter.py
+++ twittercomparator/twitter.py
@@ -19,6 +19,7 @@ def add_or_update_user(username):
     """
     Add or update a user and their tweets. 
     Return an error if not a Twitter user.
+    API limits retrieval to 250 tweets or 7 days of tweets.
     
     Parameters
     ---------------
@@ -30,18 +31,31 @@ def add_or_update_user(username):
     """
     try:
         twitter_user = TWITTER.get_user(username)
-        tweets = twitter_user.timeline(count=200, exclude_replies=True, include_rts=False, tweet_mode='extended')
-        db_user = User(id=twitter_user.id, name=twitter_user.screen_name, newest_tweet_id=tweets[0].id)
-
-        for tweet in tweets:
-            embedding = BASILICA.embed_sentence(tweet.full_text, model='twitter')
-            db_tweet = Tweet(id=tweet.id, text=tweet.full_text[:500], embedding=embedding)
-            DB.session.add(db_tweet)
-            db_user.tweets.append(db_tweet)
-
+        # Getting picture, follower count, etc. info here
+        db_user = (User.query.get(twitter_user.id) or
+                    User(id=twitter_user.id, name=username))
         DB.session.add(db_user)
-        DB.session.commit()
 
+        tweets = twitter_user.timeline(
+            count=250,
+            exclude_replies=True,
+            include_rts=False,
+            tweet_mode="extended",
+            since_id=db_user.newest_tweet_id
+        )
+
+        if tweets:
+            db_user.newest_tweet_id = tweets[0].id
+        for tweet in tweets:
+            # Grab encoding from Basilica
+            embedding = BASILICA.embed_sentence(tweet.full_text, 
+                                                model='twitter')
+            db_tweet = Tweet(id=tweet.id, text=tweet.full_text[:300], embedding=embedding)
+            # Bind tweets to user
+            db_user.tweets.append(db_tweet)
+            DB.session.add(db_tweet)
     except Exception as e:
-        print('The following exception occured: ', e)
-    
+        print("Error processing {}: {}".format(username, e))
+        raise e
+    else:
+        DB.session.commit()
